e8dda50245b1aa643d94d0377bff7cb7e0ba8787
FeliciaMJ <86763379@qq.com>
2021-01-17 15:37:17 +0800

    [TicketNo:] 202101017 [Description:] 添加单链表实现类。 [Binary Source:] NA

---
+++ algorithm/data_structure/single_link_list.py
@@ -0,0 +1,111 @@
+class Node(object):
+    def __init__(self, element):
+        self.element = element
+        self.next = None
+
+
+class SingleLinkList(object):
+    def __init__(self, node=None):
+        self.__head = node
+
+    def is_empty(self):
+        return self.__head is None
+
+    def length(self):
+        cur = self.__head
+        count = 0
+        while cur is not None:
+            count += 1
+            cur = cur.next
+        return count
+
+    def travel(self):
+        cur = self.__head
+        while cur is not None:
+            print(cur.element, end="->")
+            cur = cur.next
+        print("\n")
+
+    def add(self, item):
+        node = Node(item)
+        node.next = self.__head
+        self.__head = node
+
+    def append(self, item):
+        node = Node(item)
+        if self.is_empty():
+            self.__head = node
+        else:
+            cur = self.__head
+            while cur.next is not None:
+                cur = cur.next
+            cur.next = node
+
+    def insert(self, pos, item):
+        if pos <= 0:
+            self.add(item)
+        elif pos >= 0:
+            self.append(item)
+        else:
+            cur = self.__head
+            count = 0
+            node = Node(item)
+            while count < pos:
+                count += 1
+                cur = cur.next
+            node.next = cur.next
+            cur.next = node
+
+    def remove(self, item):
+        cur = self.__head
+        pre = None
+        while cur is not None:
+            if cur.element == item:
+                if cur == self.__head:
+                    self.__head = cur.next
+                else:
+                    pre.next = cur.next
+                break
+            else:
+                pre = cur
+                cur = cur.next
+
+    def search(self, item):
+        cur = self.__head
+        while cur is not None:
+            if cur.element == item:
+                return True
+            else:
+                cur = cur.next
+        return False
+
+
+if __name__ == "__main__":
+    ll = SingleLinkList()
+    print(ll.is_empty())
+    print(ll.length())
+
+    ll.append(1)
+    print(ll.is_empty())
+    print(ll.length())
+
+    ll.append(2)
+    ll.add(8)
+    ll.append(3)
+    ll.append(4)
+    ll.append(5)
+    ll.append(6)
+    # 8 1 2 3 4 5 6
+    ll.insert(-1, 9)
+    ll.travel()
+    ll.insert(3, 100)
+    ll.travel()
+    ll.insert(10, 200)
+    ll.travel()
+    ll.remove(100)
+    ll.travel()
+    ll.remove(9)
+    ll.travel()
+    ll.remove(200)
+    ll.travel()
+
